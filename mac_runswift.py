5c54e5cbcb0de0ba62d9b9462baadf8b75d08a79
Chris Meyer <cmeyer1969@gmail.com>
2021-03-21 09:27:38 -0700

    Simplify and document installer.

--- mac_runswift.py
+++ mac_runswift.py
@@ -1,3 +1,5 @@
+# this script is only used when building with pyinstaller.
+# pyinstaller is not currently used because it strips unused packages.
 from nionui_app.nionswift import main
 app = main.main({}, {"pyqt": None})
 app.run()
